ec372413975969fa0cb331094ffc747c6379085b
Alireza Talebizadeh <Alireza.talebizadeh78@gmail.com>
2024-02-22 12:27:37 +0330

    ...

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -3,12 +3,8 @@ import Search from "@/app/ui/dashboard/search/Search";
 import Pagination from "@/app/ui/pagination/Pagination";
 import Link from "next/link";
 import Image from "next/image";
-import { fetchUsers } from "@/app/lib/data";
 
 export default async function User() {
-  const allUsers = fetchUsers();
-
-  console.log(allUsers);
 
   return (
     <div className="bg-bgSoft p-5 rounded-[10px] mt-5">
